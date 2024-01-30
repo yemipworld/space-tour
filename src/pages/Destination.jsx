f352500a9bca21056e526d8fddf218135cc3382d
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-01-30 15:55:13 +0100

    kk

--- src/pages/Destination.jsx
+++ src/pages/Destination.jsx
@@ -1,9 +1,23 @@
 import bg from "../assets/destination/background-destination-desktop.jpg";
-import data from "../assets/data.json"
+import data from "../assets/data.json";
+import { useState } from "react";
+
 const Destination = () => {
-con
-  const p = data.destinations
-  console.log(p)
+  const [destination, setDestination] = useState(data.destinations);
+  const [index, setIndex] = useState(0);
+  const [isActive, setIsActive] = useState(0);
+
+  const current = destination[index];
+
+  console.log(typeof current);
+
+  const handleClick = (args) => {
+    setIndex(args);
+    setIsActive(args)
+
+  };
+
+  // console.log(p)
 
   return (
     <div
@@ -14,32 +28,33 @@ con
       }}
       className=" h-screen px-36 pt-24 text-white flex flex-col justify-center items-start"
     >
-      <h2><b>01</b> PICK YOUR DESTINATION</h2>
+      <h2>
+        <b>01</b> PICK YOUR DESTINATION
+      </h2>
       <div className="flex">
-        <img src="" alt="" />
+        <img src={current.images.webp} alt="" />
         <div>
-          <div className="flex  gap-2">
-            <p>mars</p>
-            <p>mars</p>
-            <p>mars</p>
-            <p>mars</p>
+          <div
+            className="flex  gap-5 barlow-condensed-regular *:cursor-pointer 
+            *:py-3 *:px-1"
+          >
+            <p className={`${isActive === 0 ? "border-b-2 border-white": ""}`} onClick={() => handleClick(0)}>MOON</p>
+            <p className={`${isActive === 1 ? "border-b-2 border-white": ""}`} onClick={() => handleClick(1)}>MARS</p>
+            <p className={`${isActive === 2 ? "border-b-2 border-white": ""}`} onClick={() => handleClick(2)}>TITAN</p>
+            <p className={`${isActive === 3 ? "border-b-2 border-white": ""}`} onClick={() => handleClick(3)}>EUROPA</p>
           </div>
           <div className="flex flex-col">
-            <h1>MARS</h1>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad
-              beatae ab maiores est repellat dolore esse quas consequatur quia
-              temporibus.
-            </p>
+            <h1 className="bellefair-regular text-3xl">{current.name}</h1>
+            <p>{current.description}</p>
           </div>
           <div className="flex">
             <div>
               <p>avg distance</p>
-              <h3>225 MIL. KM</h3>
+              <h3>{current.distance}</h3>
             </div>
             <div>
               <p>Estimated tavel</p>
-              <h3>9 months</h3>
+              <h3>{current.travel}</h3>
             </div>
           </div>
         </div>
